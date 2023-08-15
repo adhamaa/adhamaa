9b4bb4919f849b4cbc1e1faa1f63ba0c85db0ac2
Adham Akmal Azmi <adham_92@live.com>
2023-08-16 01:04:36 +0800

    build: refined

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/adhamaa",
+};
 
 module.exports = nextConfig;
