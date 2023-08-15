86655b4176b4e45a05cb2aef2cb23ad0399c37ca
Adham Akmal Azmi <adham_92@live.com>
2023-08-16 01:32:49 +0800

    ddd

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function About() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="text-3xl font-semibold capitalize p-4 border border-dashed border-teal-300">
+        About
+      </div>
+    </div>
+  );
+}
+
+export default About;
