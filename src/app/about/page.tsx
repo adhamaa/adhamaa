7dd684de4ec17c0f50b403c7bfbc3a1ba00fa07b
Adham Akmal Azmi <adham_92@live.com>
2023-08-17 12:32:24 +0800

    feat!: setup && add shadcn/ui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,10 @@
+import { Button } from "@/components/ui/button";
 import * as React from "react";
 
 function About() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="text-4xl font-semibold capitalize p-4 border border-dashed border-teal-300">
-        About
-      </div>
+      <Button>About</Button>
     </div>
   );
 }
