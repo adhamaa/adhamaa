0e5adffbbc232e6c01a220fe2249e2d797b9f6c4
Adham Akmal Azmi <adham_92@live.com>
2023-08-17 17:38:11 +0800

    fix: update release from ajiq

--- release.sh
+++ release.sh
@@ -37,3 +37,32 @@ case $strUser in
     ;;
 
 esac
+
+echo " "
+echo "Continue to merge?"
+echo "1-yes"
+echo "2-exit"
+read strUser2
+
+case $strUser2 in
+1)
+    gh pr ls
+    intMerge=$(gh pr ls | tail -n 1 | awk '{print $1}' | tr -d -c 0-9)
+    gh pr merge $intMerge
+    #error checking
+    if [ $? == 0 ]; then
+        echo "Merge success"
+    elif [ $? == 1 ]; then
+        echo "Merge failed"
+    fi
+    ;;
+
+2)
+    exit
+    ;;
+
+esac
+
+echo " "
+echo "Continue to merge?"
+ech
