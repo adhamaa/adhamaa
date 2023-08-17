44efa694731a9de1710a3022504f42241eb318a9
Adham Akmal Azmi <adham_92@live.com>
2023-08-17 16:42:44 +0800

    fix: release script

--- run.sh
+++ run.sh
@@ -2,39 +2,38 @@
 #script adham
 
 echo " "
-echo " G I T  C O M M I T "
+echo " R E A L E A S E  V E R S I O N "
 echo "1-release-as"
 echo "2-release"
 echo "ctrl-c to exit"
 read strUser
 
 case $strUser in
-        1) 
-            echo "Enter npm config version: "
-            read strNpmVer
-            git commit --allow-empty -m "chore: release %npm_config_version%" -m "release-as: $strNpmVer"
+1)
+    echo "Enter release version: "
+    read strReleaseVer
+    git commit --allow-empty -m "chore: release $strReleaseVer" -m "release-as: $strReleaseVer"
 
-            #error checking
-            if [ $? == 0 ]
-            then
-                echo "Git commit success!"
-            elif [ $? == 1 ]
-            then
-                echo "Git commit failed"
-            fi 
-            ;;
+    #error checking
+    if [ $? == 0 ]; then
+        echo "Version release success! as v$strReleaseVer"
+    elif [ $? == 1 ]; then
+        echo "Version release failed"
+    fi
+    ;;
 
-        2) 
-            git commit --allow-empty -m "chore: release"
+2)
+    git commit --allow-empty -m "chore: release"
 
-            #error checking
-            if [ $? == 0 ]
-            then
-                echo "Git commit success!"
-            elif [ $? == 1 ]
-            then
-                echo "Git commit failed"
-            fi 
-            ;;
+    #error checking
+    if [ $? == 0 ]; then
+        # get value from package.json
+        strReleaseVer=$(node -p -e "require('./package.json').version")
+
+        echo "Version v$strReleaseVer release success!"
+    elif [ $? == 1 ]; then
+        echo "Version release failed"
+    fi
+    ;;
 
 esac
