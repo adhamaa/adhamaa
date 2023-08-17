4e855117077185d9a31af201196ff60d9c47d349
Adham Akmal Azmi <adham_92@live.com>
2023-08-17 11:42:13 +0800

    feat: font-size
    release-as 0.0.5

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 function About() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="text-3xl font-semibold capitalize p-4 border border-dashed border-teal-300">
+      <div className="text-4xl font-semibold capitalize p-4 border border-dashed border-teal-300">
         About
       </div>
     </div>
