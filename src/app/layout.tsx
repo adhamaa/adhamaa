699234bc7e5e013899b69b3423f1580925061d71
Adham Akmal Azmi <adham_92@live.com>
2023-08-17 12:59:50 +0800

    feat: darkmode toggle
    chore: release

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className, "grid place-items-center")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <nav className="container m-4 px-10 w-full h-auto fixed top-0 flex items-center justify-end">
+            <ModeToggle />
+          </nav>
           {children}
         </ThemeProvider>
       </body>
